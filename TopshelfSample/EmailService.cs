afe9816c6011fcadd0956be2acc3c1f7193913be
Nishan <nishanperera@gmail.com>
2015-02-01 18:27:45 +0000

    init commit

---
+++ TopshelfSample/EmailService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace TopshelfSample
+{
+    public class EmailService 
+    {
+        private readonly Timer _timer;
+
+        public EmailService()
+        {
+            _timer = new Timer(Callback);
+        }
+
+        public void Start()
+        {
+            _timer.Change(0, 5000);
+        }
+
+        public void Stop()
+        {
+            _timer.Dispose();
+        }
+
+        private void Callback(object state)
+        {
+            Console.WriteLine("Sending emails...");
+        }
+
+    }
+
+}
+
